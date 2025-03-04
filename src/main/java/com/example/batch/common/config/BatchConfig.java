d9cdcf71cd9162d7cf9470e5c9dd9d54df4fe707
kook1932 <gkswjddyd4@naver.com>
2025-03-04 16:01:36 +0900

    [feat] add local properties

--- src/main/java/com/example/batch/common/config/BatchConfig.java
+++ src/main/java/com/example/batch/common/config/BatchConfig.java
@@ -1,9 +1,7 @@
 package com.example.batch.common.config;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @EnableBatchProcessing
 public class BatchConfig {
 }
--- src/main/java/com/example/batch/common/config/ScheduleConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.batch.common.config;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@EnableScheduling
-public class ScheduleConfig {
-}
